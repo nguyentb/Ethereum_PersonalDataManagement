b19af43195c3580cb72b0a044fa6069a26d169c9
Nguyen Truong <ngbitr@gmail.com>
2019-09-16 17:57:02 +0100

    latest

--- js/uploadData.js
+++ js/uploadData.js
@@ -2,19 +2,20 @@ function uploadData() {
 
     var dpointer = $('#dpointer').val();
     var dhash = $('#dhash').val();
+    var account = web3.eth.defaultAccount;
     
     web3.eth.getGasPrice((e, gasPrice) => {
       if (!e) {
         gasPrice = gasPrice.c[0];
         
-        contract.uploadData.estimateGas({from: web3.eth.defaultAccount}, dpointer, dhash, (er, gas) => {
+        contract.uploadData.estimateGas(account, dpointer, dhash, (er, gas) => {
           if (!er) {
             tx = {
               from: web3.eth.defaultAccount,
               gas: gas,
               gasPrice: gasPrice
             };
-            contract.uploadData.sendTransaction({from: web3.eth.defaultAccount}, dpointer, dhash, (error, result) => {
+            contract.uploadData.sendTransaction(account, dpointer, dhash, (error, result) => {
                   if (!error){
                     var a = document.createElement('a');
                     var linkText = document.createTextNode("Successfully added file.");
